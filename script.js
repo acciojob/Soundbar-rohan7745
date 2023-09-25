f2508d6a7eb904c489b6e0112a20abfbd2920574
rohan7745 <138517784+rohan7745@users.noreply.github.com>
2023-09-26 00:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const audioPlayer = document.getElementById('audioPlayer');
+
+function playAudio(soundFile) {
+    audioPlayer.src = `sounds/${soundFile}`;
+    audioPlayer.play();
+}
+
+function stopAudio() {
+    audioPlayer.pause();
+    audioPlayer.currentTime = 0;
+}
